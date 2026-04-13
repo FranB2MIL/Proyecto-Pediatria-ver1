50fffbf361d159b87d75d3780ca47af230de53ea
FranB2MIL <franciscojbollea@gmail.com>
2026-04-13 13:15:18 -0300

    Created data.js file, with a list of patients.

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const PATIENTS = [
+  {
+    id: 1,
+    nombre: "María López",
+    edad: 42,
+    sexo: "F",
+    dni: "28.450.312",
+  },
+  {
+    id: 2,
+    nombre: "Carlos Ramírez",
+    edad: 58,
+    sexo: "M",
+    dni: "14.220.889",
+  },
+  {
+    id: 3,
+    nombre: "Ana García",
+    edad: 35,
+    sexo: "F",
+    dni: "32.115.007",
+  },
+  {
+    id: 4,
+    nombre: "Jorge Pérez",
+    edad: 67,
+    sexo: "M",
+    dni: "08.930.441",
+  },
+]
