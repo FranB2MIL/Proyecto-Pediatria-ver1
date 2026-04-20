b909e07ed70b75f796a163ebcfbe839746b8a1e9
LorenzoVCC <Lorenzovecchia1@gmail.com>
2026-04-20 11:16:59 -0300

    Patient List Initial Structure Complete

--- src/components/patientList/PatientList.jsx
+++ src/components/patientList/PatientList.jsx
@@ -2,18 +2,25 @@ import styles from './PatientList.module.css'
 import PatientListItem from '../patientItem/PatientListItem'
 import { PATIENTS } from '../../data'
 
-const PatientList = ({onSelectPatient}) => {
-  
-  return (  
-    <div className={styles.patientlist}>  
-    
-      {PATIENTS.map((paciente) => (
-        <PatientListItem 
-          key={paciente.id}
-          patient={paciente}
-          onSelectPatient={onSelectPatient}
-        />
-      ))}
+const PatientList = ({ onSelectPatient }) => {
+
+  return (
+    <div className={styles.patientlist}>
+
+      <div className={styles.searchContainer}>
+        <input type="text" placeholder="Buscar paciente..." className={styles.searchInput} />
+      </div>
+      
+      <div className={styles.listContainer}>
+        {PATIENTS.map((paciente) => (
+          <PatientListItem
+            key={paciente.id}
+            patient={paciente}
+            onSelectPatient={onSelectPatient}
+          />
+        ))}
+      </div>
+
     </div>
   )
 }
